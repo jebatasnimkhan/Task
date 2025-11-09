4e52037a28577ba31e26022cb0fe6c460163f98a
jebatasnim <jebatasnimkhan@gmail.com>
2025-11-10 01:17:30 +0600

    First commit

---
+++ compare.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 30, y = 20;
+    if(x>y){
+        cout<<"x is greater than y";
+    }
+
+}
+
---
+++ task1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b, sum;
+    cout<<"Enter Two numbers : ";
+    cin>> a >> b;
+    sum = a+b;
+    cout<<endl<<"a+b="<< sum <<endl;
+    return 0;
+}
